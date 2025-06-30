968557e0e2b58b21f69bd461c6e4a0ec95c35a22
averevki <sandyverevkin@gmail.com>
2025-06-30 16:15:00 +0200

    Update

--- function_app.py
+++ function_app.py
@@ -6,6 +6,9 @@ import requests
 import azure.functions as func
 
 
+app = func.FunctionApp()
+
+
 def main(msg: func.ServiceBusMessage):
     email = msg.get_body().decode('utf-8')
     logging.info(f"Processing: {email}")
@@ -53,3 +56,8 @@ def send_email(access_token, recipient, sender):
         logging.info(f"Email sent to {recipient}")
     else:
         logging.error(f"Email failed: {response.text}")
+
+
+@app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="bussin", connection="servicebus-connection-smt")
+def servicebus_trigger(azservicebus: func.ServiceBusMessage):
+    logging.info('Python ServiceBus Queue trigger processed a message: %s', azservicebus.get_body().decode('utf-8'))
